d79542debd6134b9261848ba3557423b600e844b
lgmakena <linetmakenag@gmail.com>
2023-04-13 16:40:01 +0300

    jhj

---
+++ index.js
@@ -0,0 +1,23 @@
+// Write your code here!
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+// document.body.append(ul);
+
+// const main = document.getElementById("main")
+//  main.innerHTML = `<div>
+//   <h1> one</h1>
+//  </div>`
+
+ main.remove()
+ 
+ const newHeader = document.createElement("h1");
+
+ newHeader.textContent = "YOUR-NAME is the champion";
+
+ newHeader.setAttribute("id", "victory");
