478f04616d4b3dc988747f9cf1c7796701938071
lgmakena <linetmakenag@gmail.com>
2023-04-14 12:09:54 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+
+main.remove()
+ 
+ const newHeader = document.createElement("h1");
+
+ newHeader.textContent = "YOUR-NAME is the champion";
+
+ newHeader.setAttribute("id", "victory");
